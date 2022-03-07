208d0fcef6f13ef2676004e7bb3973fe38b224f6
Walter Gutierrez <elterrible8596@hotmail.com>
2022-03-07 15:40:23 -0400

    instalacion de npm

--- server/index.js
+++ server/index.js
@@ -1,6 +1,26 @@
 const http = require('http');
-
+const colors = require('colors');
+/*
 http.createServer(function (req, res) {
     res.write('<h1>mi primer http request</h1>');
     res.end();
-}).listen(3000)
+}).listen(3000);
+*/
+/*
+const handleServer = function (req, res) {
+    res.write('<h1>mi primer http request</h1>');
+    res.end();
+}
+
+http.createServer(handleServer).listen(3000)
+*/
+const handleServer = function (req, res) {
+    res.write('<h1>mi primer http request</h1>');
+    res.end();
+}
+
+const server = http.createServer(handleServer);
+
+server.listen(3000,function () {
+  console.log('server on port 3000'.yellow);
+})
